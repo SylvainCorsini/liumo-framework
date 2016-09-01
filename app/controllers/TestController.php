4640b020ca713a511bb52c769d438ec08f5f0cd8
SylvainCorsini <sylvain.corsini@gmail.com>
2016-09-01 22:09:41 +0200

    [Evolution] Adding default Minimis view.

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -1,17 +1,13 @@
 <?php
 namespace App\Controllers;
 
+use Http\HttpResponse;
 use Src\Controller;
 
 class TestController extends Controller
 {
     public function index()
     {
-        $users = $this->query->table('example')->select('*')->get();
-        $this->renderer->assign(array(
-                'name' => $this->request->getParameter('name', 'stranger'),
-                'users' => $users
-            ));
-        return $this->renderer->draw('test');
+        return $this->renderer->draw('welcome');
     }
 }
