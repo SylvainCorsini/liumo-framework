73b518d1205e74bd445076b036200251a21139db
SylvainCorsini <sylvain.corsini@gmail.com>
2016-09-02 19:26:27 +0200

    [Evolution] Changing config.example.

--- app/config.example.php
+++ app/config.example.php
@@ -1,22 +1,45 @@
 <?php
 
-const DEVELOPMENT_ENVIRONMENT = true;
-const DEFAULT_URI = '/minimis/public';
+/*
+ * BASE CONFIG
+ */
 
+const DEVELOPMENT_ENVIRONMENT = true; // Do not forget to change it to false on prod.
+const DEFAULT_URI = '/minimis/public';
+const CACHE_PATH = 'cache/';
+
+/*
+ * ROUTES CONFIG
+ */
+
+const ROUTES_CACHE_ENABLED = true;
+const ROUTES_CACHE_FILENAME = "routes.php";
+
+/*
+ * RENDERER CONFIG
+ */
+
+const TPL_PATH = 'app/views/';
+const ERRORS_TPL_PATH = 'errors/';
+const TPL_EXT = 'html';
 const RENDERER_SETTINGS = array(
     'checksum' => array(),
     'charset' => 'UTF-8',
     'debug' => false,
-    'tpl_dir' => '../app/views/',
-    'cache_dir' => '../cache/',
-    'tpl_ext' => 'html',
+    'tpl_dir' => '../' . TPL_PATH,
+    'cache_dir' => '../' . CACHE_PATH,
+    'tpl_ext' => TPL_EXT,
     'base_url' => DEFAULT_URI,
     'php_enabled' => false,
     'auto_escape' => true,
-    'sandbox' => true,
+    'sandbox' => true, // It is highly recommended to activate it.
     'remove_comments' => false,
     'registered_tags' => array()
 );
+
+/*
+ * DATABASE CONFIG
+ */
 
 const DB_DRIVER = 'mysql';
 const DB_CONFIG = array(
